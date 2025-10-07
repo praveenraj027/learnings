f48d5d74b96b49c6db7d746d68832a9d12480bd7
Praveen Rajak <praveenrajak0506@gmail.com>
2025-10-08 00:54:24 +0530

    Floyd's Triangle pattern

--- main.cpp
+++ main.cpp
@@ -4,12 +4,13 @@ using namespace std;
 int main()
 {
     int n = 4;
+    int num = 1;
     for (int i = 1; i <= n; i++)
     {
-        int num = 1;
-        for (int j = 1; j <= i; j++)
+
+        for (int j = i; j > 0; j--)
         {
-            cout << num;
+            cout << num << " ";
             num++;
         }
 
