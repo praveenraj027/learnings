888d650220cffa475ffc8c677bec6e262d5e40c1
Praveen Rajak <praveenrajak0506@gmail.com>
2025-10-08 21:52:57 +0530

    Sum of digits of number

--- main.cpp
+++ main.cpp
@@ -1,20 +1,22 @@
 #include <iostream>
 using namespace std;
 
-int factN(int n)
+int sumOfDigits(int num)
 {
     // int n;
     // cout << "Enter your number: ";
     // cin >> n;
-    int fact = 1;
-    for (int i = 1; i <= n; i++)
+    int digitSum = 0;
+    while (num > 0)
     {
-        fact *= i;
-    };
-    cout << "Factotial from 1 to given number is: " << fact << endl;
+        int lastDigit = num % 10;
+        num = num / 10;
+        digitSum += lastDigit;
+    }
+    cout << "Sum of digits of given number is: " << digitSum << endl;
 };
 int main()
 {
-    factN(6);
+    sumOfDigits(145);
     return 0;
 };
