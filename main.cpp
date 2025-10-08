d12a371e12f1cff2969276ed78bc4f9a0596038d
Praveen Rajak <praveenrajak0506@gmail.com>
2025-10-08 10:43:48 +0530

    Hollow diamond shape

--- main.cpp
+++ main.cpp
@@ -3,15 +3,40 @@ using namespace std;
 
 int main()
 {
-    int n = 4;
-    int num = 1;
+    int n = 5;
+    // char ch = 'A';
     for (int i = 1; i <= n; i++)
     {
-
-        for (int j = i; j > 0; j--)
+        for (int j = 1; j <= n - i; j++)
         {
-            cout << num << " ";
-            num++;
+            cout << " ";
+        }
+        cout << "*";
+        if (i != 1)
+        {
+            for (int j = 1; j <= 2 * i - 2; j++)
+            {
+                cout << " ";
+            }
+            cout << "*";
+        }
+
+        cout << endl;
+    }
+    for (int i = 1; i <= n - 1; i++)
+    {
+        for (int j = 1; j <= i; j++)
+        {
+            cout << " ";
+        }
+        cout << "*";
+        if (i != n - 1)
+        {
+            for (int j = 1; j <= 2*(n-i) - 2 ; j++)
+            {
+                cout << " ";
+            }
+            cout << "*";
         }
 
         cout << endl;
