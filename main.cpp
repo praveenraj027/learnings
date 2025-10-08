528a8ed176d4ce47b3b4ddc17cd30719273ea870
Praveen Rajak <praveenrajak0506@gmail.com>
2025-10-08 22:11:06 +0530

    Fint nCr (binomial cofficient) of the n and r

--- main.cpp
+++ main.cpp
@@ -1,22 +1,23 @@
 #include <iostream>
 using namespace std;
 
-int sumOfDigits(int num)
+int factorial(int n)
 {
-    // int n;
-    // cout << "Enter your number: ";
-    // cin >> n;
-    int digitSum = 0;
-    while (num > 0)
+    int fact = 1;
+    for (int i = 1; i <= n; i++)
     {
-        int lastDigit = num % 10;
-        num = num / 10;
-        digitSum += lastDigit;
+        fact *= i;
     }
-    cout << "Sum of digits of given number is: " << digitSum << endl;
+    return fact;
 };
 int main()
 {
-    sumOfDigits(145);
+    int n = 8, r = 2;
+    int fact_n = factorial(n);
+    int fact_r = factorial(r);
+    int fact_nr = factorial(n - r);
+
+    int nCr = fact_n / (fact_r * fact_nr);
+    cout << "Value of nCr is: " << nCr << endl;
     return 0;
 };
