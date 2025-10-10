329be91ce1ca25b8f5937f367f34451545557737
Praveen Rajak <praveenrajak0506@gmail.com>
2025-10-10 13:50:18 +0530

    Reverse of integer

--- main.cpp
+++ main.cpp
@@ -3,17 +3,14 @@ using namespace std;
 
 int main()
 {
-    int n = 10;
-    long long a = 0, b = 1, nextTerm;
-    if (n == 1){cout << 0 << endl; return 0;};
-    if (n == 2){cout << 1 << endl; return 0;};
-
-    for (int i = 3; i <= n; i++)
+    int n = 14512;
+    int rem, revNum = 0;
+    while (n > 0)
     {
-        nextTerm = a + b;
-        a = b;
-        b = nextTerm;
+        rem = n % 10;
+        revNum = revNum * 10 + rem;
+        n = n / 10;
     }
-    cout << b << endl;
+    cout << revNum;
     return 0;
 };
