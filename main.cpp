a7eb7d5f70226a045009385e43e7576b8a6c56d9
Praveen Rajak <praveenrajak0506@gmail.com>
2025-10-11 20:22:30 +0530

    Unique elements of array

--- main.cpp
+++ main.cpp
@@ -1,37 +1,30 @@
 #include <iostream>
 using namespace std;
 
-void intersectionOfArray(int array1[], int array2[], int size1, int size2)
+void unique(int array[], int size)
 {
-    for (int i = 0; i < size1; i++)
+    for (int i = 0; i < size; i++)
     {
-        for (int j = 0; j < size2; j++)
+        bool isUnique = true;
+
+        for (int j = 0; j < size; j++)
         {
-            if (array1[i] == array2[j])
+            if (i != j && array[i] == array[j])
             {
-                bool alreadyPrinted = false;
-                for (int k = 0; k < i; k++)
-                {
-                    if (array1[k] == array1[i])
-                    {
-                        alreadyPrinted = true;
-                        break;
-                    }
-                }
-                if (!alreadyPrinted)
-                {
-                    cout << array1[i] << " ";
-                }
+                isUnique = false;
+                break;
             }
+        }
+        if (isUnique)
+        {
+            cout << array[i] << " ";
         }
     }
 }
 
 int main()
 {
-    int array1[6] = {1, 2, 3, 4, 2, 2};
-    int size1 = sizeof(array1) / sizeof(int);
-    int array2[6] = {1, 23, 5, 4, 2, 6};
-    int size2 = sizeof(array2) / sizeof(int);
-    intersectionOfArray(array1, array2, size1, size2);
+    int array[6] = {1, 2, 3, 4, 2, 2};
+    int size = sizeof(array) / sizeof(int);
+    unique(array, size);
 };
