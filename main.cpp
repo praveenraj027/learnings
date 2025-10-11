0267936d6dfff529ba790f3a48b384f92eae88c1
Praveen Rajak <praveenrajak0506@gmail.com>
2025-10-11 19:50:47 +0530

    Swap min and max numbers of array

--- main.cpp
+++ main.cpp
@@ -1,20 +1,41 @@
 #include <iostream>
 using namespace std;
 
-int productOfArray(int array[], int size)
+int minOfArray(int array[], int size)
 {
-    int product = 1;
-    for (int i = 0; i < size; i++)
+    int min = array[0];
+    for (int i = 1; i < size; i++)
     {
-        product *= array[i];
+        if (array[i] < min)
+        {
+            min = array[i];
+        }
     }
-    cout << product;
+    return min;
+};
+int maxOfArray(int array[], int size)
+{
+    int max = array[0];
+    for (int i = 1; i < size; i++)
+    {
+        if (array[i] > max)
+        {
+            max = array[i];
+        }
+    }
+    return max;
 };
 
 int main()
 {
     int array[5] = {1, 2, 3, 4, 10};
     int size = sizeof(array) / sizeof(int);
-    productOfArray(array, size);
-    cout << endl;
+    int minVal = minOfArray(array, size);
+    int maxVal = maxOfArray(array, size);
+    int temp = minVal;
+    minVal = maxVal;
+    maxVal = temp;
+    cout << "Minimum number is: " << minVal << endl;
+    cout << "Maximum number is: " << maxVal;
+
 };
