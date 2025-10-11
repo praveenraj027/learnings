1e0f616d961bff7bd7b99c824823eb31904769fa
Praveen Rajak <praveenrajak0506@gmail.com>
2025-10-11 12:32:13 +0530

    Index of Largest number in array

--- main.cpp
+++ main.cpp
@@ -3,12 +3,12 @@ using namespace std;
 
 int main()
 {
-int array[5] = {12, 45, 2, 45, -3};
-int max = array[0];
+int array[5] = {12, 45, 2, 525, -3};
+int max = 0;
 int size = sizeof(array) / sizeof(int);
 for (int i = 1; i <= size; i++){
-    if(array[i] > max){
-        max = array[i];
+    if(array[i] > array[max]){
+        max = i;
     }
 }
 cout << max <<endl;
