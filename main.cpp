027e361b1f45918d76f0c007f7e9ae8808fed30f
Praveen Rajak <praveenrajak0506@gmail.com>
2025-10-11 12:33:07 +0530

    Index of Smallest number in array

--- main.cpp
+++ main.cpp
@@ -3,13 +3,13 @@ using namespace std;
 
 int main()
 {
-int array[5] = {12, 45, 2, 525, -3};
-int max = 0;
+int array[5] = {12, 45, -92, 525, -3};
+int min = 0;
 int size = sizeof(array) / sizeof(int);
 for (int i = 1; i <= size; i++){
-    if(array[i] > array[max]){
-        max = i;
+    if(array[i] < array[min]){
+        min = i;
     }
 }
-cout << max <<endl;
+cout << min <<endl;
 };
