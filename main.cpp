794a4181f9d1cf7c105daf962ed54a7ed38250e6
Praveen Rajak <praveenrajak0506@gmail.com>
2025-10-11 19:12:12 +0530

    Sum of array

--- main.cpp
+++ main.cpp
@@ -1,27 +1,20 @@
 #include <iostream>
 using namespace std;
 
-void reverseArray(int array[], int size)
+int sumOfArray(int array[], int size)
 {
-    int start = 0, end = size - 1;
-    while (start < end)
+    int sum = 0;
+    for (int i = 0; i < size; i++)
     {
-        int temp = array[start];
-        array[start] = array[end];
-        array[end] = temp;
-        start++;
-        end--;
-    };
+        sum += array[i];
+    }
+    cout << sum;
 };
 
 int main()
 {
-    int array[5] = {1, 2, 3, 4, 5};
+    int array[5] = {1, 2, 3, 4, 10};
     int size = sizeof(array) / sizeof(int);
-    reverseArray(array, size);
-    for (int i = 0; i < size; i++)
-    {
-        cout << array[i] << " ";
-    }
+    sumOfArray(array, size);
     cout << endl;
 };
