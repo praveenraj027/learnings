db232756646aac91e80d0ee4ede602e4a0384211
Praveen Rajak <praveenrajak0506@gmail.com>
2025-10-11 12:31:08 +0530

    Largest number in array

--- main.cpp
+++ main.cpp
@@ -4,12 +4,12 @@ using namespace std;
 int main()
 {
 int array[5] = {12, 45, 2, 45, -3};
-int min = array[0];
+int max = array[0];
 int size = sizeof(array) / sizeof(int);
 for (int i = 1; i <= size; i++){
-    if(array[i] < min){
-        min = array[i];
+    if(array[i] > max){
+        max = array[i];
     }
 }
-cout << min <<endl;
+cout << max <<endl;
 };
