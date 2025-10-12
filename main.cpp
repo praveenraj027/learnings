1c2a06d31fba08d86d84727cd4905f62fa482ee5
Praveen Rajak <praveenrajak0506@gmail.com>
2025-10-12 15:01:09 +0530

    Subarrays of array

--- main.cpp
+++ main.cpp
@@ -1,30 +1,21 @@
 #include <iostream>
+#include <vector>
 using namespace std;
-
-void unique(int array[], int size)
-{
-    for (int i = 0; i < size; i++)
-    {
-        bool isUnique = true;
-
-        for (int j = 0; j < size; j++)
-        {
-            if (i != j && array[i] == array[j])
-            {
-                isUnique = false;
-                break;
-            }
-        }
-        if (isUnique)
-        {
-            cout << array[i] << " ";
-        }
-    }
-}
 
 int main()
 {
-    int array[6] = {1, 2, 3, 4, 2, 2};
-    int size = sizeof(array) / sizeof(int);
-    unique(array, size);
+    int arr[5] = {1, 2, 3, 4, 5};
+    int size = sizeof(arr) / sizeof(int);
+    for (int st = 0; st < size; st++)
+    {
+        for (int end = st; end < size; end++)
+        {
+            for (int i = st; i <= end; i++)
+            {
+                cout << arr[i];
+            }
+            cout << " ";
+        }
+        cout << endl;
+    }
 };
