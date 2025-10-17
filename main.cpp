1af03824d635866f21d7c2a05d508c93164d4007
Praveen Rajak <praveenrajak0506@gmail.com>
2025-10-17 21:38:32 +0530

    Buy and Sell stock

--- main.cpp
+++ main.cpp
@@ -4,32 +4,15 @@ using namespace std;
 
 int main()
 {
-    double x = 2.22;
-    int n = 10;
-    if (n == 0)
-        return 1.0;
-    if (x == 0)
-        return 0.0;
-    if (x == -1 && n % 2 == 0)
-        return 1.0;
-    if (x == -1 && n % 2 != 0)
-        return -1.0;
-
-    long binForm = n;
-    if (n < 0)
-    {
-        x = 1 / x;
-        binForm = -binForm;
-    }
-    double ans = 1;
-    while (binForm > 0)
-    {
-        if (binForm % 2 == 1)
-        {
-            ans *= x;
+    int arr[6] = {7, 1, 5, 1, 2, 6};
+    int size = sizeof(arr) / sizeof(int);
+    int maxProfit = 0, bestBuy = arr[0];
+    for (int i = 1; i < size; i++){
+        if (arr[i] > bestBuy){
+            maxProfit = max(maxProfit, arr[i] - bestBuy);
         }
-        x *= x;
-        binForm /= 2;
+        bestBuy = min(bestBuy, arr[i]);
     }
-    cout << ans << endl;
+
+    cout << maxProfit;
 };
