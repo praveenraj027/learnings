6550b071d86dcfde485102b079bb402d2ec007d2
Praveen Rajak <praveenrajak0506@gmail.com>
2025-10-17 20:28:27 +0530

    Pow(x, n)

--- main.cpp
+++ main.cpp
@@ -4,22 +4,32 @@ using namespace std;
 
 int main()
 {
-    vector<int> vec = {1, 1, 1, 4, 5};
-    int n = vec.size();
-    int freq = 0;
-    for (int i = 0; i < n; i++)
+    double x = 2.22;
+    int n = 10;
+    if (n == 0)
+        return 1.0;
+    if (x == 0)
+        return 0.0;
+    if (x == -1 && n % 2 == 0)
+        return 1.0;
+    if (x == -1 && n % 2 != 0)
+        return -1.0;
+
+    long binForm = n;
+    if (n < 0)
     {
-        for (int j = 0; j < n; j++)
-        {
-            if (vec[i] == vec[j])
-            {
-                freq++;
-            }
-        }
-        if (freq > n / 2)
-        {
-            cout << vec[i];
-            break;
-        }
+        x = 1 / x;
+        binForm = -binForm;
     }
+    double ans = 1;
+    while (binForm > 0)
+    {
+        if (binForm % 2 == 1)
+        {
+            ans *= x;
+        }
+        x *= x;
+        binForm /= 2;
+    }
+    cout << ans << endl;
 };
