a72d9f2e7e5b30b90572a4b7dd10479504524f71
Praveen Rajak <praveenrajak0506@gmail.com>
2025-10-19 13:04:23 +0530

    Product of array except itself - brute force

--- main.cpp
+++ main.cpp
@@ -4,18 +4,23 @@ using namespace std;
 
 int main()
 {
-    int height[9] = {1, 8, 6, 2, 5, 4, 8, 3, 7};
-    int n = sizeof(height) / sizeof(int);
-    int maxWater = 0;
-    int lp = 0, rp = n - 1;
-    while (lp < rp)
+    vector<int> arr = {1, 2, 3, 4, 5};
+    int n = arr.size();
+    vector<int> ans(n, 1);
+    for (int i = 0; i < n; i++)
     {
-        int w = rp - lp;
-        int h = min(height[lp], height[rp]);
-        int area = w * h;
-        maxWater = max(area, maxWater);
-
-        height[lp] < height[rp] ? lp++ : rp--;
+        for (int j = 0; j < n; j++)
+        {
+            if (i != j)
+            {
+                ans[i] *= arr[j];
+            }
+        }
     }
-    cout << maxWater;
+    cout << "[" ;
+    for (int i = 0; i < n; i++)
+    {
+        cout << ans[i] << ", ";
+    }
+    cout << "]" ;
 };
