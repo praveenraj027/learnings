07e630c134b1dd6c432a563449cb63db9cc4e13f
Praveen Rajak <praveenrajak0506@gmail.com>
2025-10-19 20:53:08 +0530

    Product of array except itself - O(n)

--- main.cpp
+++ main.cpp
@@ -4,23 +4,33 @@ using namespace std;
 
 int main()
 {
-    vector<int> arr = {1, 2, 3, 4, 5};
+    vector<int> arr = {1, 2, 3, 4};
     int n = arr.size();
     vector<int> ans(n, 1);
+    vector<int> prefix(n, 1);
+    vector<int> suffix(n, 1);
+    for (int i = 1; i < n; i++)
+    {
+        prefix[i] = prefix[i - 1] * arr[i - 1];
+    }
+    for (int i = n - 2; i >= 0; i--)
+    {
+        suffix[i] = suffix[i + 1] * arr[i + 1];
+    }
+
     for (int i = 0; i < n; i++)
     {
-        for (int j = 0; j < n; j++)
+        ans[i] = prefix[i] * suffix[i];
+    }
+    cout << "[";
+    for (int i = 0; i < n; i++)
+    {
+        if (i != n - 1)
         {
-            if (i != j)
-            {
-                ans[i] *= arr[j];
-            }
+            cout << ans[i] << ", ";
+        }else{
+            cout << ans[i];
         }
     }
-    cout << "[" ;
-    for (int i = 0; i < n; i++)
-    {
-        cout << ans[i] << ", ";
-    }
-    cout << "]" ;
+    cout << "]" << endl;
 };
