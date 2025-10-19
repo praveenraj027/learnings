9dd29643b10fe50e4cd90461f22f53237af351fd
Praveen Rajak <praveenrajak0506@gmail.com>
2025-10-19 21:30:09 +0530

    Product of array except itself - O(n) with optimised space complexity

--- main.cpp
+++ main.cpp
@@ -4,23 +4,18 @@ using namespace std;
 
 int main()
 {
-    vector<int> arr = {1, 2, 3, 4};
+    vector<int> arr = {1, 6, 3, 4};
     int n = arr.size();
     vector<int> ans(n, 1);
-    vector<int> prefix(n, 1);
-    vector<int> suffix(n, 1);
     for (int i = 1; i < n; i++)
     {
-        prefix[i] = prefix[i - 1] * arr[i - 1];
+        ans[i] = ans[i - 1] * arr[i - 1];
     }
+    int suffix = 1;
     for (int i = n - 2; i >= 0; i--)
     {
-        suffix[i] = suffix[i + 1] * arr[i + 1];
-    }
-
-    for (int i = 0; i < n; i++)
-    {
-        ans[i] = prefix[i] * suffix[i];
+        suffix *= arr[i + 1];
+        ans[i] *= suffix;
     }
     cout << "[";
     for (int i = 0; i < n; i++)
