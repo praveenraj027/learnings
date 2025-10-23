aa48f6bab6ae77a3c82a7c9e2a7537cf59ea2b8e
Praveen Rajak <praveenrajak0506@gmail.com>
2025-10-23 21:52:06 +0530

    Binary search Algo - for even no. of elements

--- main.cpp
+++ main.cpp
@@ -4,28 +4,34 @@ using namespace std;
 
 int main()
 {
-    vector<int> arr = {1, 6, 3, 4};
-    int n = arr.size();
-    vector<int> ans(n, 1);
-    for (int i = 1; i < n; i++)
-    {
-        ans[i] = ans[i - 1] * arr[i - 1];
-    }
-    int suffix = 1;
-    for (int i = n - 2; i >= 0; i--)
-    {
-        suffix *= arr[i + 1];
-        ans[i] *= suffix;
-    }
-    cout << "[";
-    for (int i = 0; i < n; i++)
-    {
-        if (i != n - 1)
-        {
-            cout << ans[i] << ", ";
-        }else{
-            cout << ans[i];
-        }
-    }
-    cout << "]" << endl;
+   vector<int> arr1 = {-1, 0, 5, 6, 9, 74};
+   int tar1 = 9;
+   vector<int> arr2 = {1, 2, 4, 6, 89};
+   int tar2 = 6;
+   int n = arr1.size();
+
+   int st = 0, end = n - 1;
+   bool found = false;
+   while (st <= end)
+   {
+      int mid = (st + end) / 2;
+      if (tar1 > arr1[mid])
+      {
+         st = mid + 1;
+      }
+      else if (tar1 < arr1[mid])
+      {
+         end = mid - 1;
+      }
+      else
+      {
+         cout << arr1[mid];
+         found = true;
+         break;
+      }
+   }
+   if (!found)
+   {
+      cout << "Element not found!" << endl;
+   }
 };
