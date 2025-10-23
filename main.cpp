89b53fe20fff97210ce99111e469b8a4371c0093
Praveen Rajak <praveenrajak0506@gmail.com>
2025-10-23 21:52:33 +0530

    Binary search Algo - for odd no. of elements

--- main.cpp
+++ main.cpp
@@ -5,27 +5,27 @@ using namespace std;
 int main()
 {
    vector<int> arr1 = {-1, 0, 5, 6, 9, 74};
-   int tar1 = 9;
+   int tar1 = 90;
    vector<int> arr2 = {1, 2, 4, 6, 89};
-   int tar2 = 6;
-   int n = arr1.size();
+   int tar2 = 89;
+   int n = arr2.size();
 
    int st = 0, end = n - 1;
    bool found = false;
    while (st <= end)
    {
       int mid = (st + end) / 2;
-      if (tar1 > arr1[mid])
+      if (tar2 > arr2[mid])
       {
          st = mid + 1;
       }
-      else if (tar1 < arr1[mid])
+      else if (tar2 < arr2[mid])
       {
          end = mid - 1;
       }
       else
       {
-         cout << arr1[mid];
+         cout << arr2[mid];
          found = true;
          break;
       }
