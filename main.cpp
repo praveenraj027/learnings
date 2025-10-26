1dbc0f9cdcab99b4945ea5f7a7b58e07da521d73
Praveen Rajak <praveenrajak0506@gmail.com>
2025-10-26 20:58:32 +0530

    Single element in array right approach with edge cases

--- main.cpp
+++ main.cpp
@@ -4,12 +4,26 @@ using namespace std;
 
 int main()
 {
-   vector<int> arr = {1, 1, 2, 2, 3, 4, 4, 5, 5};
-   int st = 1, end = arr.size() - 2;
-   bool found = false;
+   vector<int> arr = {2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7};
+   if (arr.size() == 1)
+   {
+      cout << arr[0];
+      return 0;
+   };
+   int st = 0, end = arr.size() - 1;
    while (st <= end)
    {
       int mid = st + (end - st) / 2;
+      if (mid == 0 && arr[0] != arr[mid + 1])
+      {
+         cout << arr[mid];
+         break;
+      };
+      if (mid == end && arr[end] != arr[mid - 1])
+      {
+         cout << arr[mid];
+         break;
+      };
       if (arr[mid - 1] != arr[mid] && arr[mid] != arr[mid + 1])
       {
          cout << arr[mid];
@@ -17,7 +31,7 @@ int main()
       }
       if (mid % 2 == 0)
       {
-         if (arr[mid - 1] = arr[mid])
+         if (arr[mid - 1] == arr[mid])
          {
             end = mid - 1;
          }
@@ -28,7 +42,7 @@ int main()
       }
       else
       {
-         if (arr[mid - 1] = arr[mid])
+         if (arr[mid - 1] == arr[mid])
          {
             st = mid + 1;
          }
