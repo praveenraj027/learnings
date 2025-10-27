e8ef31a40d866e8cdacf38aa703b4a4bf5234cf0
Praveen Rajak <praveenrajak0506@gmail.com>
2025-10-27 20:30:38 +0530

    Book allocation problem (Google interview question)

--- main.cpp
+++ main.cpp
@@ -2,54 +2,58 @@
 #include <vector>
 using namespace std;
 
-int main()
+bool isValid(vector<int> &arr, int n, int m, int maxAllowedPages)
 {
-   vector<int> arr = {2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7};
-   if (arr.size() == 1)
+   int stu = 1, pages = 0;
+   for (int i = 0; i < n; i++)
    {
-      cout << arr[0];
-      return 0;
-   };
-   int st = 0, end = arr.size() - 1;
-   while (st <= end)
-   {
-      int mid = st + (end - st) / 2;
-      if (mid == 0 && arr[0] != arr[mid + 1])
+      if (arr[i] > maxAllowedPages)
       {
-         cout << arr[mid];
-         break;
-      };
-      if (mid == end && arr[end] != arr[mid - 1])
-      {
-         cout << arr[mid];
-         break;
-      };
-      if (arr[mid - 1] != arr[mid] && arr[mid] != arr[mid + 1])
-      {
-         cout << arr[mid];
-         break;
+         return false;
       }
-      if (mid % 2 == 0)
+      if (pages + arr[i] <= maxAllowedPages)
       {
-         if (arr[mid - 1] == arr[mid])
-         {
-            end = mid - 1;
-         }
-         else
-         {
-            st = mid + 1;
-         }
+         pages += arr[i];
       }
       else
       {
-         if (arr[mid - 1] == arr[mid])
-         {
-            st = mid + 1;
-         }
-         else
-         {
-            end = mid - 1;
-         }
+         stu++;
+         pages = arr[i];
       }
    }
+
+   return stu > m ? false : true;
+}
+
+int allocateBooks(vector<int> &arr, int n, int m)
+{
+   int sum = 0;
+   for (int i = 0; i < arr.size(); i++)
+   {
+      sum += arr[i];
+   }
+   int st = 0, end = sum;
+   int ans = -1;
+
+   while (st <= end)
+   {
+      int mid = st + (end - st) / 2;
+      if (isValid(arr, n, m, mid))
+      {
+         ans = mid;
+         end = mid - 1;
+      }
+      else
+      {
+         st = mid + 1;
+      }
+   }
+   return ans;
+}
+
+int main()
+{
+   vector<int> arr = {2, 1, 3, 4};
+   int n = 4, m = 2;
+   cout << allocateBooks(arr, n, m) << endl;
 };
