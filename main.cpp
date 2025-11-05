da9029f89f7d11dbb07ef2eb0c3e6158af065df3
Praveen Rajak <praveenrajak0506@gmail.com>
2025-11-06 00:52:43 +0530

    Recerse a CharArray

--- main.cpp
+++ main.cpp
@@ -1,54 +1,19 @@
 #include <iostream>
 #include <vector>
+#include <cstring>
 #include <algorithm>
 using namespace std;
-vector<int> nextPermutation(vector<int> &arr, int n)
-{
-   int pivot = -1;
-   for (int i = n - 2; i >= 0; i--)
-   {
-      if (arr[i] < arr[i + 1])
-      {
-         pivot = i;
-         break;
-      }
-   }
-   if (pivot == -1)
-   {
-      reverse(arr.begin(), arr.end());
-      return arr;
-   }
-
-   for (int i = n - 1; i > pivot; i--)
-   {
-      if (arr[i] > arr[pivot])
-      {
-         swap(arr[i], arr[pivot]);
-         break;
-      }
-   }
-
-   int i = pivot + 1, j = n - 1;
-   while (i <= j)
-   {
-      swap(arr[i], arr[j]);
-      i++;
-      j--;
-   }
-   return arr;
-}
-void printArr(vector<int> arr, int n)
-{
-   for (int i = 0; i < n; i++)
-   {
-      cout << arr[i] << " ";
-   }
-}
 
 int main()
 {
-   vector<int> arr = {1, 2, 5, 4, 6};
-   int n = arr.size();
-   nextPermutation(arr, n);
-   printArr(arr, n);
+   char str[] = "Piya";
+   int st = 0, end = strlen(str) - 1;
+   while (st < end){
+      swap(str[st], str[end]);
+      st++;
+      end--;
+   }
+   for (char val : str){
+      cout << val << " ";
+   }
 };
