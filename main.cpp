972ac8f8ea4f34aaf065f9ae43c92427de157646
Praveen Rajak <praveenrajak0506@gmail.com>
2025-11-11 19:51:19 +0530

    Linear search in 2d array

--- main.cpp
+++ main.cpp
@@ -4,27 +4,26 @@
 #include <algorithm>
 using namespace std;
 
+bool linear2D(int matrix[][3], int rows, int columns, int target)
+{
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            if (matrix[i][j] == target)
+            {
+                cout << "Target " << target << " found in the 2d array at (" << i << ", " << j << ")" << endl;
+                return 0;
+            }
+        }
+    }
+    cout << "Target doesn't exist in matrix" << endl;
+}
+
 int main()
 {
-    int matrix[4][3];
+    int matrix[4][3] = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}, {10, 11, 12}};
     int rows = 4;
     int columns = 3;
-    // Input
-    for (int i = 0; i < rows; i++)
-    {
-        for (int j = 0; j < columns; j++)
-        {
-            cin >> matrix[i][j];
-        }
-    }
-
-    // Output
-    for (int i = 0; i < rows; i++)
-    {
-        for (int j = 0; j < columns; j++)
-        {
-            cout << matrix[i][j] << " ";
-        }
-        cout << endl;
-    }
+    linear2D(matrix, rows, columns, 12);
 }
