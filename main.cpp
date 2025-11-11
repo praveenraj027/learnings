c4be5dc1541d873113ab0d34a09408028fd28f1e
Praveen Rajak <praveenrajak0506@gmail.com>
2025-11-11 19:40:08 +0530

    Taking input and printing 2D array (matrix)

--- main.cpp
+++ main.cpp
@@ -6,9 +6,19 @@ using namespace std;
 
 int main()
 {
-    int matrix[4][3] = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}, {10, 11, 12}};
+    int matrix[4][3];
     int rows = 4;
     int columns = 3;
+    // Input
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+            cin >> matrix[i][j];
+        }
+    }
+
+    // Output
     for (int i = 0; i < rows; i++)
     {
         for (int j = 0; j < columns; j++)
