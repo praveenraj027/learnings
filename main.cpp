51f288bfd5228bf7aada9e37805cbb73424b5535
Praveen Rajak <praveenrajak0506@gmail.com>
2025-11-11 22:53:51 +0530

    Maximum row sum in 2d array

--- main.cpp
+++ main.cpp
@@ -4,34 +4,26 @@
 #include <algorithm>
 using namespace std;
 
-pair<int, int> linear2D(int matrix[][3], int rows, int columns, int target)
+int linear2D(int matrix[][3], int rows, int columns)
 {
+    int maxRowSum = 0;
     for (int i = 0; i < rows; i++)
     {
+        int rowSum = 0;
         for (int j = 0; j < columns; j++)
         {
-            if (matrix[i][j] == target)
-            {
-                return {i, j};
-            }
+            rowSum += matrix[i][j];
         }
+        maxRowSum = max(rowSum, maxRowSum);
     }
-    return {-1, -1};
+
+    cout << maxRowSum << endl;
 }
 
 int main()
 {
-    int matrix[4][3] = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}, {10, 11, 12}};
+    int matrix[4][3] = {{1, 2, 31}, {4, 5, 6}, {7, 8, 9}, {10, 11, 12}};
     int rows = 4;
     int columns = 3;
-    pair<int, int> result = linear2D(matrix, rows, columns, 12);
-
-    if (result.first != -1)
-    {
-        cout << "Target found at (" << result.first << ", " << result.second << ")" << endl;
-    }
-    else
-    {
-        cout << "Target doesn't exist" << endl;
-    }
+    linear2D(matrix, rows, columns);
 }
