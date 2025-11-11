f95d66bdef16f844a8d3702bf233d802390e727b
Praveen Rajak <praveenrajak0506@gmail.com>
2025-11-11 22:35:58 +0530

    Linear search in 2d array returning pair of indices

--- main.cpp
+++ main.cpp
@@ -4,7 +4,7 @@
 #include <algorithm>
 using namespace std;
 
-bool linear2D(int matrix[][3], int rows, int columns, int target)
+pair<int, int> linear2D(int matrix[][3], int rows, int columns, int target)
 {
     for (int i = 0; i < rows; i++)
     {
@@ -12,12 +12,11 @@ bool linear2D(int matrix[][3], int rows, int columns, int target)
         {
             if (matrix[i][j] == target)
             {
-                cout << "Target " << target << " found in the 2d array at (" << i << ", " << j << ")" << endl;
-                return 0;
+                return {i, j};
             }
         }
     }
-    cout << "Target doesn't exist in matrix" << endl;
+    return {-1, -1};
 }
 
 int main()
@@ -25,5 +24,14 @@ int main()
     int matrix[4][3] = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}, {10, 11, 12}};
     int rows = 4;
     int columns = 3;
-    linear2D(matrix, rows, columns, 12);
+    pair<int, int> result = linear2D(matrix, rows, columns, 12);
+
+    if (result.first != -1)
+    {
+        cout << "Target found at (" << result.first << ", " << result.second << ")" << endl;
+    }
+    else
+    {
+        cout << "Target doesn't exist" << endl;
+    }
 }
