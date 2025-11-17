d7b56b835c0e3facc79a1a3d771945b414687717
Praveen Rajak <praveenrajak0506@gmail.com>
2025-11-17 10:40:23 +0530

    ThreeSome (3Sum) closest

--- main.cpp
+++ main.cpp
@@ -3,22 +3,27 @@
 #include <vector>
 #include <algorithm>
 using namespace std;
-// ThreeSome (3Sum);
-vector<vector<int>> threeSum(vector<int> &nums)
+// ThreeSome (3Sum) closest;
+int threeSumClosest(vector<int> &nums, int target)
 {
     int n = nums.size();
     sort(nums.begin(), nums.end());
-    vector<vector<int>> ans;
+    int bestSum = nums[0] + nums[1] + nums[2];
+    int bestDiff = abs(bestSum - target);
 
     for (int i = 0; i < n; i++)
     {
-        if (i > 0 && nums[i] == nums[i - 1])
-            continue;
         int j = i + 1, k = n - 1;
 
         while (j < k)
         {
             int sum = nums[i] + nums[j] + nums[k];
+            int diff = abs(sum - target);
+            if (diff < bestDiff)
+            {
+                bestDiff = diff;
+                bestSum = sum;
+            }
             if (sum < 0)
             {
                 j++;
@@ -29,30 +34,17 @@ vector<vector<int>> threeSum(vector<int> &nums)
             }
             else
             {
-                ans.push_back({nums[i], nums[j], nums[k]});
-                j++;
-                k--;
-
-                while (j < k && nums[j] == nums[j - 1])
-                    j++;
+                cout << target << endl;
+                return 0;
             }
         }
     }
-    return ans;
+    cout << bestSum << endl;
 }
 
 int main()
 {
-    vector<int> arr = {-1, 0, 1, 2, -1, -4};
-    vector<vector<int>> result = threeSum(arr);
-    for (int i = 0; i < result.size(); i++)
-    {
-        cout << "[";
-        for (int j = 0; j < result[i].size(); j++)
-        {
-            cout << result[i][j] << " ";
-        }
-        cout << "]";
-        cout << endl;
-    }
+    vector<int> arr = {-1, 2, 1, -4};
+    int target = 1;
+    threeSumClosest(arr, target);
 }
