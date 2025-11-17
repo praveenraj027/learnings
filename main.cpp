3307aa731463df24bfe13f0f450bf80a1f5c1c1b
Praveen Rajak <praveenrajak0506@gmail.com>
2025-11-17 10:16:49 +0530

    ThreeSome

--- main.cpp
+++ main.cpp
@@ -4,23 +4,55 @@
 #include <algorithm>
 using namespace std;
 // Find Duplicate
-
-int findDuplicate(vector<int> nums)
+vector<vector<int>> threeSum(vector<int> &nums)
 {
-    for (int i = 0; i < nums.size(); i++)
+    int n = nums.size();
+    sort(nums.begin(), nums.end());
+    vector<vector<int>> ans;
+
+    for (int i = 0; i < n; i++)
     {
-        for (int j = i + 1; j < nums.size(); j++)
+        if (i > 0 && nums[i] == nums[i - 1])
+            continue;
+        int j = i + 1, k = n - 1;
+
+        while (j < k)
         {
-            if (nums[i] == nums[j])
+            int sum = nums[i] + nums[j] + nums[k];
+            if (sum < 0)
             {
-                cout << nums[i] << endl;
-                return 0;
+                j++;
+            }
+            else if (sum > 0)
+            {
+                k--;
+            }
+            else
+            {
+                ans.push_back({nums[i], nums[j], nums[k]});
+                j++;
+                k--;
+
+                while (j < k && nums[j] == nums[j - 1])
+                    j++;
             }
         }
     }
+    return ans;
 }
+
 int main()
 {
-    vector<int> arr = {1, 2, 2, 3, 4, 5};
-    findDuplicate(arr);
+    vector<int> arr = {-1, 0, 1, 2, -1, -4};
+    vector<vector<int>> result = threeSum(arr);
+    for (int i = 0; i < result.size(); i++)
+    {
+        cout << "[";
+        for (int j = 0; j < result[i].size(); j++)
+        {
+            cout << result[i][j] << " ";
+        }
+        cout << "]";
+        cout << endl;
+    }
 }
