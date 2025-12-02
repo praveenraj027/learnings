351bc33dc8cd0fad52fcceccf6424760c67a9c7a
Praveen Rajak <praveenrajak0506@gmail.com>
2025-12-02 22:32:22 +0530

    Quick sort - Decreasing form..

--- main.cpp
+++ main.cpp
@@ -7,7 +7,7 @@ int partition(vector<int>& arr, int st, int end){
     int idx = st - 1, pivot = arr[end];
 
     for(int j = st; j < end; j++){
-        if(arr[j] <= pivot){
+        if(arr[j] >= pivot){
             idx++;
             swap(arr[j], arr[idx]);
         }
@@ -29,7 +29,7 @@ void quickSort(vector<int> &arr, int st, int end)
 
 int main()
 {
-    vector<int> arr = {5, 4, 3, 2, 1};
+    vector<int> arr = {45, 445, 1, 846, 48, 3};
     quickSort(arr, 0, arr.size() - 1);
 
     for (int val : arr){
