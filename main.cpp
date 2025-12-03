a8178b695aa2968a090832c9011ecd2cd67c8b01
Praveen Rajak <praveenrajak0506@gmail.com>
2025-12-03 17:04:09 +0530

    Sum of all digits of integer using recursion

--- main.cpp
+++ main.cpp
@@ -3,38 +3,14 @@
 #include <algorithm>
 using namespace std;
 
-int partition(vector<int>& arr, int st, int end){
-    int idx = st - 1, pivot = arr[end];
-
-    for(int j = st; j < end; j++){
-        if(arr[j] >= pivot){
-            idx++;
-            swap(arr[j], arr[idx]);
-        }
-    }
-    idx++;
-    swap(arr[idx], arr[end]);
-    return idx;
-}
-
-void quickSort(vector<int> &arr, int st, int end)
+int sumDig(int n)
 {
-    if(st < end){
-        int pvtIdx = partition(arr, st, end);
-
-        quickSort(arr, st, pvtIdx - 1); //leftcall
-        quickSort(arr, pvtIdx + 1, end); //rightcall
-    }
+    if (n < 10)
+        return n;
+    return n % 10 + sumDig(n / 10);
 }
 
 int main()
 {
-    vector<int> arr = {45, 445, 1, 846, 48, 3};
-    quickSort(arr, 0, arr.size() - 1);
-
-    for (int val : arr){
-        cout << val << " ";
-    }
-    cout << endl;
-    return 0;
+    cout << sumDig(532);
 }
