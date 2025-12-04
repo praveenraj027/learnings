3d06012d59780ea68f95bed1387704d01aa165fe
Praveen Rajak <praveenrajak0506@gmail.com>
2025-12-04 21:49:02 +0530

    Inversion count

--- main.cpp
+++ main.cpp
@@ -3,20 +3,63 @@
 #include <algorithm>
 using namespace std;
 
-int countInversion(vector<int>& nums)
+int merge(vector<int> &arr, int st, int end, int mid)
 {
-    int count = 0;
-    for (int i = 0; i < nums.size(); i++){
-        for (int j = i; j < nums.size(); j++){
-            if (i < j && nums[i] > nums[j]){
-                count++;
-            }
+    vector<int> temp;
+    int i = st, j = mid + 1;
+    int invCount = 0;
+
+    while (i <= mid && j <= end)
+    {
+        if (arr[i] < arr[j])
+        {
+            temp.push_back(arr[i]);
+            i++;
+        }
+        else
+        {
+            temp.push_back(arr[j]);
+            j++;
+            invCount += (mid - i + 1);
         }
     }
-    return count;
+    while (i <= mid)
+    {
+        temp.push_back(arr[i]);
+        i++;
+    }
+
+    while (j <= end)
+    {
+        temp.push_back(arr[j]);
+        j++;
+    }
+
+    for (int idx = 0; idx < temp.size(); idx++)
+    {
+        arr[idx + st] = temp[idx];
+    }
+    return invCount;
 }
+
+int mergeSort(vector<int> &arr, int st, int end)
+{
+    if (st < end)
+    {
+        int mid = st + (end - st) / 2;
+
+        int leftInvCount = mergeSort(arr, st, mid);
+        int rightInvCount = mergeSort(arr, mid + 1, end);
+
+        int invCount = merge(arr, st, end, mid);
+
+        return leftInvCount + rightInvCount + invCount;
+    }
+    return 0;
+}
+
 int main()
 {
-    vector<int> arr = {6, 3, 5, 2, 7};
-    cout << countInversion(arr);
+    vector<int> arr = {1, 3, 5, 10, 2, 6, 8, 9};
+    cout << mergeSort(arr, 0, arr.size() - 1);
 }
