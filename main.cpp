6d2857f69ebdd9bfe996021f1e08f6d0dcefa2f9
Praveen Rajak <praveenrajak0506@gmail.com>
2025-12-05 21:52:40 +0530

    Custom copy constructor

--- main.cpp
+++ main.cpp
@@ -12,12 +12,22 @@ public:
     string name;
     string dept;
     string subject;
+    // Parameterized constructor
     Teacher(string name, string dept, string subject, double salary)
     {
         this->name = name;
         this->dept = dept;
         this->subject = subject;
         this->salary = salary;
+    }
+    // Custom copy constructor
+    Teacher(Teacher &orgObj)
+    {
+        cout << "I am a custom copy sonstructor" << endl;
+        this->name = orgObj.name;
+        this->dept = orgObj.dept;
+        this->subject = orgObj.subject;
+        this->salary = orgObj.salary;
     }
     // methods / member functions
 
@@ -40,7 +50,7 @@ public:
 int main()
 {
     Teacher t1("Piyuuu", "AIML", "OOPS", 12050);
-    Teacher t2(t1); //default copy constructor
+    Teacher t2(t1); // default copy constructor
 
     t2.display();
     return 0;
