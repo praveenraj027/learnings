d4f659a8996993985fd3030468712ddeca15b6e4
Praveen Rajak <praveenrajak0506@gmail.com>
2025-12-05 21:28:23 +0530

    Encapsulation

--- main.cpp
+++ main.cpp
@@ -1,24 +1,41 @@
 #include <iostream>
-#include <vector>
-#include <algorithm>
+#include <string>
 using namespace std;
 
-bool isSorted(vector<int> &arr, int i, int n){
-    if (i == n - 1) return true;
-    if (arr[i] >= arr[i + 1]){
-        return false;
-    }   
-    return isSorted(arr, i + 1, n);
-}
+class Teacher
+{
+private:
+    double salary;
+
+public:
+    // properties / attributes
+    string name;
+    string dept;
+    string subject;
+    // methods / member functions
+
+    void changeDept(string newDept)
+    {
+        dept = newDept;
+    }
+
+    void setSalary(double s)
+    {
+        salary = s;
+    }
+
+    void displaySalary()
+    {
+        cout << "Salary: " << salary;
+    }
+};
 
 int main()
 {
-    vector<int> arr = {1, 2, 3, 4};
-    if (isSorted(arr, 0, arr.size())){
-        cout << "Sorted" << endl;
-    }
-    else
-    {
-        cout << "Not sorted" << endl;
-    }
+    Teacher t1;
+    t1.name = "Piyu";
+    cout << t1.name << endl;
+    t1.setSalary(15000);
+    t1.displaySalary();
+    return 0;
 }
