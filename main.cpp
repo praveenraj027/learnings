d27504ce7cfe892b65453e68eb6684074c98df57
Praveen Rajak <praveenrajak0506@gmail.com>
2025-12-05 21:30:57 +0530

    Constructor

--- main.cpp
+++ main.cpp
@@ -8,6 +8,10 @@ private:
     double salary;
 
 public:
+    Teacher()
+    {
+        salary = 12500;
+    }
     // properties / attributes
     string name;
     string dept;
@@ -26,16 +30,18 @@ public:
 
     void displaySalary()
     {
-        cout << "Salary: " << salary;
+        cout << "Salary: " << salary << endl;
     }
 };
 
 int main()
 {
     Teacher t1;
+    Teacher t2;
     t1.name = "Piyu";
     cout << t1.name << endl;
     t1.setSalary(15000);
     t1.displaySalary();
+    t2.displaySalary();
     return 0;
 }
