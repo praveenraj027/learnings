1f61768eb8d8d9a73eb82a2009bd8279cf7e58cd
Praveen Rajak <praveenrajak0506@gmail.com>
2025-12-09 22:10:29 +0530

    Wrote pop_back, insert, and search functions

--- main.cpp
+++ main.cpp
@@ -51,21 +51,77 @@ public:
         else
         {
             tail->next = newNode;
-            tail = NULL;
+            tail = newNode;
         }
     }
 
     void pop_front()
     {
+        if (head == NULL)
+        {
+            cout << "List is empty, nothing to delete." << endl;
+            return;
+        }
         Node *temp = head;
         head = head->next;
         temp->next = NULL;
         delete temp;
     }
+    void pop_back()
+    {
+        if (head == NULL)
+        {
+            cout << "List is empty, nothing to delete." << endl;
+            return;
+        }
+        if (head == tail)
+        {
+            delete head;
+            head = tail = NULL;
+            return;
+        }
+        Node *temp = head;
+        while (temp->next != tail)
+        {
+            temp = temp->next;
+        }
+
+        temp->next = NULL;
+        delete tail;
+        tail = temp;
+    }
+
+    void insert(int val, int pos)
+    {
+        if (pos < 0)
+        {
+            cout << "Invalid position.." << endl;
+            return;
+        }
+        if (pos == 0)
+        {
+            push_front(val);
+            return;
+        }
+
+        Node *temp = head;
+        for (int i = 0; i < pos - 1; i++)
+        {
+            temp = temp->next;
+        }
+        Node *newNode = new Node(val);
+        newNode->next = temp->next;
+        temp->next = newNode;
+    }
 
     void printLL()
     {
         Node *temp = head;
+        if (head == NULL)
+        {
+            cout << "List is empty." << endl;
+            return;
+        }
         while (temp != NULL)
         {
             cout << temp->data << " -> ";
@@ -73,18 +129,33 @@ public:
         }
         cout << "NULL" << endl;
     }
+    void search(int key)
+    {
+        Node *temp = head;
+        if (head == NULL)
+        {
+            cout << "List is empty." << endl;
+            return;
+        }
+        int idx = 0;
+        while (temp != NULL)
+        {
+            if (temp->data == key)
+            {
+                cout << key << " is found at index: " << idx << endl;
+                return;
+            }
+            temp = temp->next;
+            idx++;
+        }
+        cout << endl;
+    }
 };
 
 int main()
 {
     List ll;
-    ll.push_front(1);
-    ll.push_front(2);
-    ll.printLL();
-    ll.push_front(3);
-    ll.printLL();
-    ll.push_back(12);
-    ll.pop_front();
+    ll.search(1);
     ll.printLL();
     return 0;
 }
