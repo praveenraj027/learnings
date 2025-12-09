a19c096b2164d1a9526af7e9a9933a2c5149395c
Praveen Rajak <praveenrajak0506@gmail.com>
2025-12-09 20:40:02 +0530

    Creating and Printing a linked list

--- main.cpp
+++ main.cpp
@@ -2,23 +2,62 @@
 #include <string>
 using namespace std;
 
-class Shape
-{
-    virtual void draw() = 0;
-};
-
-class Circle : public Shape
+class Node
 {
 public:
-    void draw()
+    int data;
+    Node *next;
+
+    Node(int val)
     {
-        cout << "Drawing a circle\n";
+        data = val;
+        next = NULL;
+    }
+};
+
+class List
+{
+    Node *head;
+    Node *tail;
+
+public:
+    List()
+    {
+        head = tail = NULL;
+    }
+
+    void push_front(int val)
+    {
+        Node *newNode = new Node(val);
+        if (head == NULL)
+        {
+            head = tail = newNode;
+            return;
+        }
+        else
+        {
+            newNode->next = head;
+            head = newNode;
+        }
+    }
+
+    void printLL()
+    {
+        Node *temp = head;
+        while (temp != NULL)
+        {
+            cout << temp->data << " ";
+            temp = temp->next;
+        }
     }
 };
 
 int main()
 {
-    Circle s1;
-    s1.draw();
+    List ll;
+    ll.push_front(1);
+    ll.push_front(2);
+    ll.push_front(3);
+    ll.printLL();
     return 0;
 }
