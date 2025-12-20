715e9927db62557596a81aa26135b334faab9fa5
Praveen Rajak <praveenrajak0506@gmail.com>
2025-12-20 21:13:24 +0530

    Stack using c++ STL

--- main.cpp
+++ main.cpp
@@ -1,32 +1,11 @@
 #include <iostream>
-#include <list>
+#include <stack>
 using namespace std;
 
-class Stack
-{
-    list<int> ll;
-public:
-    void push(int val)
-    {
-        ll.push_front(val);
-    }
-
-    void pop()
-    {
-        ll.pop_front();
-    }
-    int top()
-    {
-        return ll.front();
-    }
-    bool empty(){
-        return ll.size() == 0;
-    }
-};
 
 int main()
 {
-    Stack s;
+    stack<int> s;
     s.push(12);
     s.push(1);
     s.push(2);
