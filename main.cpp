6d4adfc27794d192c5ae994e1638eeb638f2c3b1
Praveen Rajak <praveenrajak0506@gmail.com>
2025-12-27 21:31:23 +0530

    The celebrity problem

--- main.cpp
+++ main.cpp
@@ -3,27 +3,47 @@
 #include <vector>
 using namespace std;
 
+int getCelebrity(vector<vector<int>> &arr)
+{
+    int n = arr.size();
+    stack<int> s;
+    for (int i = 0; i < n; i++)
+    {
+        s.push(i);
+    }
+
+    while (s.size() > 1)
+    {
+        int i = s.top();
+        s.pop();
+        int j = s.top();
+        s.pop();
+
+        if (arr[i][j] == 0)
+        {
+            s.push(i);
+        }
+        else
+        {
+            s.push(j);
+        }
+    }
+    int celeb = s.top();
+
+    for (int i = 0; i < n; i++)
+    {
+        if ((i != celeb) && (arr[i][celeb] == 0 || arr[celeb][i] == 1))
+        {
+            return -1;
+        }
+    }
+    return celeb;
+}
+
 int main()
 {
-    vector<int> nums = {6, 8, 0, 1, 3};
-    stack<int> s;
-    vector<int> ans(nums.size());
-
-    for (int i = 0; i < nums.size(); i++){
-        while(s.size() > 0 && s.top() >= nums[i]){
-            s.pop();
-        }
-        if (s.empty()) {
-            ans[i] = -1;
-        }
-        else{
-            ans[i] = s.top();
-        }
-        s.push(nums[i]);
-    }
-
-    for (int val : ans){
-        cout << val << " ";
-    }
+    vector<vector<int>> arr = {{0, 1, 0}, {0, 0, 0}, {0, 1, 0}};
+    int ans = getCelebrity(arr);
+    cout << "Celebrity is: " << ans << endl;
     return 0;
 }
