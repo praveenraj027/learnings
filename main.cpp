b74b9b1ea573a93787699647b2b770a959be089b
Praveen Rajak <praveenrajak0506@gmail.com>
2025-12-28 17:25:32 +0530

    Queue structure using linked list

--- main.cpp
+++ main.cpp
@@ -1,49 +1,29 @@
 #include <iostream>
-#include <stack>
 #include <vector>
 using namespace std;
 
-int getCelebrity(vector<vector<int>> &arr)
+class Node
 {
-    int n = arr.size();
-    stack<int> s;
-    for (int i = 0; i < n; i++)
-    {
-        s.push(i);
+public:
+    int data;
+    Node *next;
+
+    Node(int val){
+        data = val;
+        next = NULL;
     }
+};
+class Queue
+{
+    Node* head;
+    Node* tail;
 
-    while (s.size() > 1)
-    {
-        int i = s.top();
-        s.pop();
-        int j = s.top();
-        s.pop();
-
-        if (arr[i][j] == 0)
-        {
-            s.push(i);
-        }
-        else
-        {
-            s.push(j);
-        }
+    public:
+    Queue(){
+        head = tail = NULL;
     }
-    int celeb = s.top();
-
-    for (int i = 0; i < n; i++)
-    {
-        if ((i != celeb) && (arr[i][celeb] == 0 || arr[celeb][i] == 1))
-        {
-            return -1;
-        }
-    }
-    return celeb;
-}
-
+};
 int main()
 {
-    vector<vector<int>> arr = {{0, 1, 0}, {0, 0, 0}, {0, 1, 0}};
-    int ans = getCelebrity(arr);
-    cout << "Celebrity is: " << ans << endl;
     return 0;
 }
