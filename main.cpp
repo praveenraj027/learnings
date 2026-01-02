aaa58fd03b098d6318d3ae714286d8bf58be2763
Praveen Rajak <praveenrajak0506@gmail.com>
2026-01-02 21:49:04 +0530

    Binary tree levelorder traversal technique

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <vector>
+#include <queue>
 using namespace std;
 
 class Node
@@ -53,11 +54,31 @@ void postorderTraversal(Node *root)
     postorderTraversal(root->right);
     cout << root->data << " ";
 }
+void levelorderTraversal(Node *root)
+{
+    queue<Node *> q;
+    q.push(root);
+    while (q.size() > 0)
+    {
+        Node *curr = q.front();
+        q.pop();
+
+        cout << curr->data << " ";
+        if (curr->left != NULL)
+        {
+            q.push(curr->left);
+        }
+        if (curr->right != NULL)
+        {
+            q.push(curr->right);
+        }
+    }
+}
 
 int main()
 {
     vector<int> preorder = {1, 2, -1, -1, 3, 4, -1, -1, 5, -1, -1};
     Node *root = buildTree(preorder);
-    postorderTraversal(root);
+    levelorderTraversal(root);
     return 0;
 }
