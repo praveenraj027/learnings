50565130536a9d6ddf7c67057357c02e3270ae84
Praveen Rajak <praveenrajak0506@gmail.com>
2026-01-04 23:09:46 +0530

    Kth level of the binary tree

--- main.cpp
+++ main.cpp
@@ -31,45 +31,24 @@ Node *buildTree(vector<int> &preorder)
     return root;
 }
 
-void topView(Node *root)
-{
-    queue<pair<Node *, int>> q;
-    map<int, int> m;
-    q.push({root, 0});
-
-    while (q.size() > 0)
-    {
-        Node *curr = q.front().first;
-        int currHD = q.front().second;
-        q.pop();
-
-        if (m.find(currHD) == m.end())
-        {
-            m[currHD] = curr->data;
-        }
-
-        if (curr->left != NULL)
-        {
-            q.push({curr->left, currHD - 1});
-        }
-
-        if (curr->right != NULL)
-        {
-            q.push({curr->right, currHD + 1});
-        }
+void kthLevel(Node* root, int k){
+    if (root == NULL){
+        return;
     }
 
-    for (auto it : m)
-    {
-        cout << it.second << " ";
+    if (k == 1){
+        cout << root->data << " ";
+        return;
     }
-    cout << endl;
+
+    kthLevel(root->left, k - 1);
+    kthLevel(root->right, k - 1);
 }
 
 int main()
 {
     vector<int> preorder = {1, 2, 6, -1, -1, -1, 3, 4, -1, -1, 5, -1, -1};
     Node *root = buildTree(preorder);
-    topView(root);
+        kthLevel(root, 3);
     return 0;
 }
