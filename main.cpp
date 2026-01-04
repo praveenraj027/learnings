48e391cb7ab1ff596f414c6c266fa582426f9b30
Praveen Rajak <praveenrajak0506@gmail.com>
2026-01-04 23:03:53 +0530

    Top view of the binary tree

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
 #include <queue>
+#include <map>
 using namespace std;
 
 class Node
@@ -30,20 +31,45 @@ Node *buildTree(vector<int> &preorder)
     return root;
 }
 
-int sum(Node *root)
+void topView(Node *root)
 {
-    if (root == NULL)
-        return 0;
-    int leftSum = sum(root->left);
-    int rightSum = sum(root->right);
+    queue<pair<Node *, int>> q;
+    map<int, int> m;
+    q.push({root, 0});
 
-    return leftSum + rightSum + root->data;
+    while (q.size() > 0)
+    {
+        Node *curr = q.front().first;
+        int currHD = q.front().second;
+        q.pop();
+
+        if (m.find(currHD) == m.end())
+        {
+            m[currHD] = curr->data;
+        }
+
+        if (curr->left != NULL)
+        {
+            q.push({curr->left, currHD - 1});
+        }
+
+        if (curr->right != NULL)
+        {
+            q.push({curr->right, currHD + 1});
+        }
+    }
+
+    for (auto it : m)
+    {
+        cout << it.second << " ";
+    }
+    cout << endl;
 }
 
 int main()
 {
     vector<int> preorder = {1, 2, 6, -1, -1, -1, 3, 4, -1, -1, 5, -1, -1};
     Node *root = buildTree(preorder);
-    cout << sum(root);
+    topView(root);
     return 0;
 }
