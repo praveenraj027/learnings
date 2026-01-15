41df5cada40a3dd76651164cc2a114c5e01d7329
Praveen Rajak <praveenrajak0506@gmail.com>
2026-01-15 23:18:25 +0530

    Breadth-first search

--- main.cpp
+++ main.cpp
@@ -1,32 +1,64 @@
 #include <iostream>
 #include <vector>
 #include <list>
+#include <queue>
 using namespace std;
 
-class Graph{
+class Graph
+{
     int V;
-    list<int> *l; //Like dynamic arr (int *arr;)
+    list<int> *l; // Like dynamic arr (int *arr;)
 
-    public:
-    Graph(int V){
+public:
+    Graph(int V)
+    {
         this->V = V;
-        l = new list<int> [V];
-        // arr = new arr[V];    
+        l = new list<int>[V];
+        // arr = new arr[V];
     }
 
-    void addEdge(int u, int v){
+    void addEdge(int u, int v)
+    {
         l[u].push_back(v);
         l[v].push_back(u);
     }
 
-    void printAdjList(){
-        for (int i = 0; i < V; i++){
+    void printAdjList()
+    {
+        for (int i = 0; i < V; i++)
+        {
             cout << i << ": ";
-            for (int neigh : l[i]){
+            for (int neigh : l[i])
+            {
                 cout << neigh << " ";
             }
             cout << endl;
         }
+    }
+
+    void bfs()
+    {
+        queue<int> q;
+        vector<bool> vis(V, false);
+        q.push(0);
+        vis[0] = true;
+
+        while (q.size() > 0)
+        {
+            int u = q.front();
+            q.pop();
+            cout << u << " ";
+
+            for (int v : l[u])
+            {
+                if (!vis[v])
+                {
+                    vis[v] = true;
+                    q.push(v);
+                }
+            }
+        }
+        cout << endl;
     }
 };
 
@@ -38,6 +70,7 @@ int main()
     g.addEdge(1, 3);
     g.addEdge(2, 3);
     g.addEdge(2, 4);
-    g.printAdjList();
+    // g.printAdjList();
+    g.bfs();
     return 0;
 }
