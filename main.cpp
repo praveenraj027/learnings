cb0ab6d1462ffaeb85680415092a26c01e810f58
Praveen Rajak <praveenrajak0506@gmail.com>
2026-01-16 17:15:30 +0530

    Cycle detection in an undirected graph

--- main.cpp
+++ main.cpp
@@ -88,19 +88,51 @@ public:
         }
         cout << endl;
     }
+
+    bool isCycleUndirDFS(int src, int par, vector<bool> &vis)
+    {
+        vis[src] = true;
+        list<int> neighbours = l[src];
+
+        for (int v : neighbours)
+        {
+            if (!vis[v])
+            {
+                if (isCycleUndirDFS(v, src, vis))
+                    return true;
+            }
+            else if (v != par)
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    bool isCycle()
+    {
+        int par = -1;
+        vector<bool> vis(V, false);
+        for (int i = 0; i < V; i++)
+        {
+            if (!vis[i])
+            {
+                if (isCycleUndirDFS(i, par, vis))
+                    return true;
+            }
+        }
+        return false;
+    }
 };
 
 int main()
 {
     Graph g(5);
     g.addEdge(0, 1);
+    g.addEdge(0, 2);
+    g.addEdge(0, 3);
     g.addEdge(1, 2);
-    g.addEdge(1, 3);
-    g.addEdge(2, 4);
-    // g.printAdjList();
-    cout << "DFS: ";
-    g.dfs();
-    cout << "BFS: ";
-    g.bfs();
+    g.addEdge(3, 4);
+    cout << g.isCycle() << endl;
     return 0;
 }
