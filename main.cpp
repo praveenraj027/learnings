aff231561fa7a7afa66152841d6d126b9138f583
Praveen Rajak <praveenrajak0506@gmail.com>
2026-01-16 16:52:12 +0530

    Depth-first Search

--- main.cpp
+++ main.cpp
@@ -60,6 +60,29 @@ public:
         }
         cout << endl;
     }
+
+    void dfsHelper(int u, vector<bool> &vis)
+    {
+        cout << u << " ";
+        vis[u] = true;
+
+        for (int v : l[u])
+        {
+            if (!vis[v])
+            {
+                dfsHelper(v, vis);
+            }
+        }
+    }
+
+    void dfs()
+    {
+        int u = 0;
+        vector<bool> vis(V, false);
+
+        dfsHelper(u, vis);
+        cout << endl;
+    }
 };
 
 int main()
@@ -68,9 +91,11 @@ int main()
     g.addEdge(0, 1);
     g.addEdge(1, 2);
     g.addEdge(1, 3);
-    g.addEdge(2, 3);
     g.addEdge(2, 4);
     // g.printAdjList();
+    cout << "DFS: ";
+    g.dfs();
+    cout << "BFS: ";
     g.bfs();
     return 0;
 }
