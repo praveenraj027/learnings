c5561484c8dfa60d9c725a9961696e43408d1210
Praveen Rajak <praveenrajak0506@gmail.com>
2026-01-26 21:58:10 +0530

    Bellman Ford's algorithm

--- main.cpp
+++ main.cpp
@@ -45,22 +45,43 @@ void dijktra(int src, vector<vector<Edge>> &g, int V)
     cout << endl;
 }
 
+void bellmanFord(int src, vector<vector<Edge>> &g, int V)
+{
+    vector<int> dist(V, INT_MAX);
+    dist[src] = 0;
+    for (int i = 0; i < V - 1; i++)
+    {
+        for (int u = 0; u < V; u++)
+        {
+            for (Edge e : g[u])
+            {
+                if (dist[e.v] > dist[u] + e.wt)
+                {
+                    dist[e.v] = dist[u] + e.wt;
+                }
+            }
+        }
+    }
+
+    for (int i = 0; i < V; i++){
+        cout << dist[i] << " ";
+    }
+    cout << endl;
+}
+
 int main()
 {
-    int V = 6;
+    int V = 5;
     vector<vector<Edge>> g(V);
     g[0].push_back(Edge(1, 2));
     g[0].push_back(Edge(2, 4));
 
-    g[1].push_back(Edge(2, 1));
-    g[1].push_back(Edge(3, 7));
+    g[1].push_back(Edge(2, -4));
+    g[1].push_back(Edge(4, -1));
 
-    g[2].push_back(Edge(4, 3));
+    g[2].push_back(Edge(3, 2));
 
-    g[3].push_back(Edge(5, 1));
-
-    g[4].push_back(Edge(5, 5));
-    g[4].push_back(Edge(3, 2));
-    dijktra(0, g, V);
+    g[3].push_back(Edge(4, 4));
+    bellmanFord(0, g, V);
     return 0;
 }
