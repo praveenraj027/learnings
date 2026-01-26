a0289b69affa6ed215c326296b6350d5d7a67634
Praveen Rajak <praveenrajak0506@gmail.com>
2026-01-26 21:39:05 +0530

    Dijktra's algorithm

--- main.cpp
+++ main.cpp
@@ -1,95 +1,66 @@
 #include <iostream>
 #include <vector>
-#include <list>
+#include <climits>
 #include <queue>
 using namespace std;
 
-class Graph
+class Edge
 {
-    int V;
-    list<int> *l; // Like dynamic arr (int *arr;)
-
 public:
-    Graph(int V)
+    int v;
+    int wt;
+
+    Edge(int v, int wt)
     {
-        this->V = V;
-        l = new list<int>[V];
-        // arr = new arr[V];
-    }
-
-    void addEdge(int u, int v)
-    {
-        l[u].push_back(v);
-        // l[v].push_back(u);
-    }
-
-    void printAdjList()
-    {
-        for (int i = 0; i < V; i++)
-        {
-            cout << i << ": ";
-            for (int neigh : l[i])
-            {
-                cout << neigh << " ";
-            }
-            cout << endl;
-        }
-    }
-
-    void topologicalSort()
-    {
-        vector<int> result;
-        vector<int> indegree(V, 0);
-
-        // Calculate indegree
-        for (int u = 0; u < V; u++)
-        {
-            for (int v : l[u])
-            {
-                indegree[v]++;
-            }
-        }
-        // 0 indegree => queue
-        queue<int> q;
-        for (int i = 0; i < V; i++)
-        {
-            if (indegree[i] == 0)
-                q.push(i);
-        }
-
-        while (!q.empty())
-        {
-            int curr = q.front();
-            q.pop();
-            result.push_back(curr);
-            for (int v : l[curr])
-            {
-                indegree[v]--;
-                if (indegree[v] == 0)
-                {
-                    q.push(v);
-                }
-            }
-        }
-
-        // Print result
-        for (int val : result)
-        {
-            cout << val << " ";
-        }
-        cout << endl;
+        this->v = v;
+        this->wt = wt;
     }
 };
 
+void dijktra(int src, vector<vector<Edge>> &g, int V)
+{
+    vector<int> dist(V, INT_MAX);
+    dist[src] = 0;
+    priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
+    pq.push({dist[src], src});
+    while (pq.size() > 0)
+    {
+        int u = pq.top().second;
+        pq.pop();
+
+        for (Edge e : g[u])
+        {
+            if (dist[e.v] > dist[u] + e.wt)
+            {
+                dist[e.v] = dist[u] + e.wt;
+                pq.push({dist[e.v], e.v});
+            }
+        }
+    }
+
+    for (int i = 0; i < V; i++)
+    {
+        cout << dist[i] << " ";
+    }
+    cout << endl;
+}
+
 int main()
 {
-    Graph g(6);
-    g.addEdge(3, 1);
-    g.addEdge(2, 3);
-    g.addEdge(4, 0);
-    g.addEdge(4, 1);
-    g.addEdge(5, 0);
-    g.addEdge(5, 2);
-    g.topologicalSort();
+    int V = 6;
+    vector<vector<Edge>> g(V);
+    g[0].push_back(Edge(1, 2));
+    g[0].push_back(Edge(2, 4));
+
+    g[1].push_back(Edge(2, 1));
+    g[1].push_back(Edge(3, 7));
+
+    g[2].push_back(Edge(4, 3));
+
+    g[3].push_back(Edge(5, 1));
+
+    g[4].push_back(Edge(5, 5));
+    g[4].push_back(Edge(3, 2));
+    dijktra(0, g, V);
     return 0;
 }
