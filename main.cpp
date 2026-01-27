c991e34cea1ca562311eddced916de66a911991f
Praveen Rajak <praveenrajak0506@gmail.com>
2026-01-27 10:59:45 +0530

    Shortest path in DAG

--- main.cpp
+++ main.cpp
@@ -1,87 +1,113 @@
 #include <iostream>
 #include <vector>
+#include <stack>
+#include <list>
+#include <unordered_map>
 #include <climits>
-#include <queue>
 using namespace std;
 
-class Edge
+class Graph
 {
 public:
-    int v;
-    int wt;
+    unordered_map<int, list<pair<int, int>>> adj;
 
-    Edge(int v, int wt)
+    void addEdge(int u, int v, int wt)
     {
-        this->v = v;
-        this->wt = wt;
+        pair<int, int> p = make_pair(v, wt);
+        adj[u].push_back(p);
     }
-};
 
-void dijktra(int src, vector<vector<Edge>> &g, int V)
-{
-    vector<int> dist(V, INT_MAX);
-    dist[src] = 0;
-    priority_queue<pair<int, int>, vector<pair<int, int>>, greater<pair<int, int>>> pq;
-    pq.push({dist[src], src});
-    while (pq.size() > 0)
+    void printAdj()
     {
-        int u = pq.top().second;
-        pq.pop();
-
-        for (Edge e : g[u])
+        for (auto i : adj)
         {
-            if (dist[e.v] > dist[u] + e.wt)
+            cout << i.first << "->";
+            for (auto j : i.second)
             {
-                dist[e.v] = dist[u] + e.wt;
-                pq.push({dist[e.v], e.v});
+                cout << "(" << j.first << ", " << j.second << "), ";
             }
+            cout << endl;
         }
     }
 
-    for (int i = 0; i < V; i++)
+    void dfs(int src, unordered_map<int, bool> &vis, stack<int> &s)
     {
-        cout << dist[i] << " ";
-    }
-    cout << endl;
-}
-
-void bellmanFord(int src, vector<vector<Edge>> &g, int V)
-{
-    vector<int> dist(V, INT_MAX);
-    dist[src] = 0;
-    for (int i = 0; i < V - 1; i++)
-    {
-        for (int u = 0; u < V; u++)
+        vis[src] = true;
+        for (auto neighbour : adj[src])
         {
-            for (Edge e : g[u])
+            if (!vis[neighbour.first])
             {
-                if (dist[e.v] > dist[u] + e.wt)
+                dfs(neighbour.first, vis, s);
+            }
+        }
+        s.push(src);
+    }
+
+    void getShortestPath(int src, vector<int> &dist, stack<int> &s)
+    {
+        dist[src] = 0;
+
+        while (s.size() > 0)
+        {
+            int top = s.top();
+            s.pop();
+
+            if (dist[top] != INT_MAX)
+            {
+                for (auto i : adj[top])
                 {
-                    dist[e.v] = dist[u] + e.wt;
+                    if (dist[top] + i.second < dist[i.first])
+                    {
+                        dist[i.first] = dist[top] + i.second;
+                    }
                 }
             }
         }
     }
-
-    for (int i = 0; i < V; i++){
-        cout << dist[i] << " ";
-    }
-    cout << endl;
-}
+};
 
 int main()
 {
-    int V = 5;
-    vector<vector<Edge>> g(V);
-    g[0].push_back(Edge(1, 2));
-    g[0].push_back(Edge(2, 4));
+    Graph g;
+    g.addEdge(0, 1, 5);
+    g.addEdge(0, 2, 3);
+    g.addEdge(1, 2, 2);
+    g.addEdge(1, 3, 6);
+    g.addEdge(2, 3, 7);
+    g.addEdge(2, 4, 4);
+    g.addEdge(2, 5, 2);
+    g.addEdge(3, 4, -1);
+    g.addEdge(4, 5, -2);
+    g.printAdj();
 
-    g[1].push_back(Edge(2, -4));
-    g[1].push_back(Edge(4, -1));
+    // topological sort
+    unordered_map<int, bool> vis;
+    stack<int> s;
+    int n = 6;
+    for (int i = 0; i < n; i++)
+    {
+        if (!vis[i])
+        {
+            g.dfs(i, vis, s);
+        }
+    }
 
-    g[2].push_back(Edge(3, 2));
+    int src = 1;
+    vector<int> dist(n, INT_MAX);
 
-    g[3].push_back(Edge(4, 4));
-    bellmanFord(0, g, V);
+    g.getShortestPath(src, dist, s);
+    cout << "Answer is: " << endl;
+    for (int i = 0; i < dist.size(); i++)
+    {
+        if (dist[i] == INT_MAX)
+        {
+            cout << "Infinity" << " ";
+        }
+        else
+        {
+            cout << dist[i] << " ";
+        }
+    }
+    cout << endl;
     return 0;
 }
