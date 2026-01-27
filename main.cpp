58acc75b2991f1f5f6c58ac833f6da7236aa8260
Praveen Rajak <praveenrajak0506@gmail.com>
2026-01-27 23:12:25 +0530

    Shortest path in undirected graphs

--- main.cpp
+++ main.cpp
@@ -3,111 +3,74 @@
 #include <stack>
 #include <list>
 #include <unordered_map>
-#include <climits>
+#include <queue>
+#include <algorithm>
 using namespace std;
 
-class Graph
+void shortestPath(vector<pair<int, int>> edges, int n, int m, int s, int t)
 {
-public:
-    unordered_map<int, list<pair<int, int>>> adj;
-
-    void addEdge(int u, int v, int wt)
+    unordered_map<int, list<int>> adj;
+    for (int i = 0; i < edges.size(); i++)
     {
-        pair<int, int> p = make_pair(v, wt);
-        adj[u].push_back(p);
+        int u = edges[i].first;
+        int v = edges[i].second;
+
+        adj[u].push_back(v);
+        adj[v].push_back(u);
     }
 
-    void printAdj()
+    // bfs
+    unordered_map<int, bool> vis;
+    unordered_map<int, int> par;
+    queue<int> q;
+    q.push(s);
+    vis[s] = true;
+
+    while (!q.empty())
     {
-        for (auto i : adj)
+        int front = q.front();
+        q.pop();
+
+        for (auto i : adj[front])
         {
-            cout << i.first << "->";
-            for (auto j : i.second)
+            if (!vis[i])
             {
-                cout << "(" << j.first << ", " << j.second << "), ";
-            }
-            cout << endl;
-        }
-    }
-
-    void dfs(int src, unordered_map<int, bool> &vis, stack<int> &s)
-    {
-        vis[src] = true;
-        for (auto neighbour : adj[src])
-        {
-            if (!vis[neighbour.first])
-            {
-                dfs(neighbour.first, vis, s);
-            }
-        }
-        s.push(src);
-    }
-
-    void getShortestPath(int src, vector<int> &dist, stack<int> &s)
-    {
-        dist[src] = 0;
-
-        while (s.size() > 0)
-        {
-            int top = s.top();
-            s.pop();
-
-            if (dist[top] != INT_MAX)
-            {
-                for (auto i : adj[top])
-                {
-                    if (dist[top] + i.second < dist[i.first])
-                    {
-                        dist[i.first] = dist[top] + i.second;
-                    }
-                }
+                vis[i] = true;
+                par[i] = front;
+                q.push(i);
             }
         }
     }
-};
+    // prepare shortest path
+
+    vector<int> ans;
+    int curr = t;
+    ans.push_back(t);
+    while (curr != s)
+    {
+        curr = par[curr];
+        ans.push_back(curr);
+    }
+    reverse(ans.begin(), ans.end());
+    for (int val : ans)
+    {
+        cout << val << " ";
+    }
+    cout << endl;
+}
 
 int main()
 {
-    Graph g;
-    g.addEdge(0, 1, 5);
-    g.addEdge(0, 2, 3);
-    g.addEdge(1, 2, 2);
-    g.addEdge(1, 3, 6);
-    g.addEdge(2, 3, 7);
-    g.addEdge(2, 4, 4);
-    g.addEdge(2, 5, 2);
-    g.addEdge(3, 4, -1);
-    g.addEdge(4, 5, -2);
-    g.printAdj();
-
-    // topological sort
-    unordered_map<int, bool> vis;
-    stack<int> s;
-    int n = 6;
-    for (int i = 0; i < n; i++)
-    {
-        if (!vis[i])
-        {
-            g.dfs(i, vis, s);
-        }
-    }
-
-    int src = 1;
-    vector<int> dist(n, INT_MAX);
-
-    g.getShortestPath(src, dist, s);
-    cout << "Answer is: " << endl;
-    for (int i = 0; i < dist.size(); i++)
-    {
-        if (dist[i] == INT_MAX)
-        {
-            cout << "Infinity" << " ";
-        }
-        else
-        {
-            cout << dist[i] << " ";
-        }
-    }
-    cout << endl;
+    vector<pair<int, int>> edges = {
+        {1, 2},
+        {2, 3},
+        {3, 4},
+        {1, 4},
+        {4, 5}};
+    int n = 5;
+    int m = 5;
+    int s = 1;
+    int t = 5;
+    shortestPath(edges, n, m, s, t);
     return 0;
 }
