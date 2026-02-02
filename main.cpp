307081afde8cb5a3b89c44a1d146e8d2cfb4cc00
Praveen Rajak <praveenrajak0506@gmail.com>
2026-02-02 22:53:30 +0530

    Heapify algorithm

--- main.cpp
+++ main.cpp
@@ -82,6 +82,27 @@ public:
     }
 };
 
+void heapify(int arr[], int n, int i)
+{
+    int largest = i;
+    int left = 2 * i;
+    int right = 2 * i + 1;
+
+    if (left < n && arr[largest] < arr[left])
+    {
+        largest = left;
+    }
+    if (right < n && arr[largest] < arr[right])
+    {
+        largest = right;
+    }
+
+    if (largest != i){
+        swap(arr[largest], arr[i]);
+        heapify(arr, n, largest);
+    }
+}
+
 int main()
 {
     Heap h;
@@ -89,8 +110,20 @@ int main()
     h.insert(55);
     h.insert(52);
     h.insert(40);
-    h.print();
+    // h.print();
     h.deleteFromHeap();
-    h.print();
+    // h.print();
+
+    int arr[6] = {-1, 54, 53, 55, 52, 50};
+    int n = 5;
+    for (int i = n / 2; i > 0; i--){
+        heapify(arr, n, i);
+    }
+
+    cout << "Printing the heap: " << endl;
+    for (int i = 1; i <= n; i++){
+        cout << arr[i] << " ";
+    }
+    cout << endl;
     return 0;
 }
