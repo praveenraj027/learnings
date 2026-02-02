ae1d95b7077e5eed111826b017a48387817d8ba4
Praveen Rajak <praveenrajak0506@gmail.com>
2026-02-02 23:11:19 +0530

    Heap sort

--- main.cpp
+++ main.cpp
@@ -59,7 +59,7 @@ public:
         size--;
 
         int i = 1;
-        while (i < size)
+        while (i <= size)
         {
             int leftIdx = 2 * i;
             int rightIdx = 2 * i + 1;
@@ -84,22 +84,35 @@ public:
 
 void heapify(int arr[], int n, int i)
 {
-    int smallest = i;
-    int left = 2 * i + 1;
-    int right = 2 * i + 2;
+    int largest = i;
+    int left = 2 * i;
+    int right = 2 * i + 1;
 
-    if (left < n && arr[smallest] > arr[left])
+    if (left <= n && arr[largest] < arr[left])
     {
-        smallest = left;
+        largest = left;
     }
-    if (right < n && arr[smallest] > arr[right])
+    if (right <= n && arr[largest] < arr[right])
     {
-        smallest = right;
+        largest = right;
     }
 
-    if (smallest != i){
-        swap(arr[smallest], arr[i]);
-        heapify(arr, n, smallest);
+    if (largest != i)
+    {
+        swap(arr[largest], arr[i]);
+        heapify(arr, n, largest);
+    }
+}
+
+void heapSort(int arr[], int n)
+{
+    int size = n;
+
+    while (size > 1)
+    {
+        swap(arr[size], arr[1]);
+        size--;
+        heapify(arr, size, 1);
     }
 }
 
@@ -114,14 +127,16 @@ int main()
     h.deleteFromHeap();
     // h.print();
 
-    int arr[6] = {60, 54, 53, 55, 52, 50};
-    int n = 6;
-    for (int i = n / 2; i >= 0; i--){
+    int arr[6] = {-1, 54, 53, 55, 52, 50};
+    int n = 5;
+    for (int i = n / 2; i > 0; i--)
+    {
         heapify(arr, n, i);
     }
-
+    heapSort(arr, n);
     cout << "Printing the heap: " << endl;
-    for (int i = 0; i < n; i++){
+    for (int i = 1; i <= n; i++)
+    {
         cout << arr[i] << " ";
     }
     cout << endl;
