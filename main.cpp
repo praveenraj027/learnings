16ab89b23182a32f138aaa49d98f879533563116
Praveen Rajak <praveenrajak0506@gmail.com>
2026-02-08 10:53:22 +0530

    Printing map elements using for loop and iterator

--- main.cpp
+++ main.cpp
@@ -6,32 +6,23 @@ using namespace std;
 int main()
 {
     unordered_map<string, int> m;
-    pair<string, int> p = make_pair("piyu", 100);
-    m.insert(p);
     pair<string, int> pair2("praveen", 12);
     m.insert(pair2);
+    pair<string, int> p = make_pair("piyu", 100);
+    m.insert(p);
 
     m["romeo"] = 23;
 
-    // search 
-    cout << m["romeo"] << endl;
-    cout << m.at("piyu") << endl;
+    // printing map elements using for each loop
+    for (auto i : m){
+        cout << i.first << " -> " << i.second << endl;
+    }
 
-    cout << m["anything"] << endl;
-    cout << m.at("anything") << endl;
-
-    //size
-
-    cout << m.size() << endl;
-
-    //to check presence
-
-    cout << m.count("piyu") << endl;
-    cout << m.count("piyuu") << endl;
-
-    //erase
-
-    m.erase("praveen");
-    cout << m.count("praveen") << endl;
+    //printing map elements using iterator
+    unordered_map<string, int> :: iterator it = m.begin();
+    while (it != m.end()){
+        cout << it->first << " -> " << it->second << endl;
+        it++;
+    }
     return 0;
 }
