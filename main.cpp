3743cb28f0e46994ba25f89e68a072d3a1a1cc12
Praveen Rajak <praveenrajak0506@gmail.com>
2026-02-08 10:44:08 +0530

    Some basic operations of hasmaps

--- main.cpp
+++ main.cpp
@@ -1,156 +1,37 @@
 #include <iostream>
-#include <vector>
-#include <climits>
-#include <list>
+#include <map>
 #include <unordered_map>
-#include <queue>
-#include <stack>
-#include <algorithm>
 using namespace std;
-
-class Heap
-{
-public:
-    int arr[100];
-    int size;
-    Heap()
-    {
-        arr[0] = -1;
-        size = 0;
-    }
-    void insert(int val)
-    {
-        size = size + 1;
-        int index = size;
-        arr[index] = val;
-
-        while (index > 1)
-        {
-            int parent = index / 2;
-            if (arr[parent] < arr[index])
-            {
-                swap(arr[parent], arr[index]);
-                index = parent;
-            }
-            else
-            {
-                return;
-            }
-        }
-    }
-
-    void print()
-    {
-        for (int i = 1; i <= size; i++)
-        {
-            cout << arr[i] << " ";
-        }
-        cout << endl;
-    }
-
-    void deleteFromHeap()
-    {
-        if (size == 0)
-        {
-            cout << "Nothing to delete.." << endl;
-            return;
-        }
-        arr[1] = arr[size];
-        size--;
-
-        int i = 1;
-        while (i <= size)
-        {
-            int leftIdx = 2 * i;
-            int rightIdx = 2 * i + 1;
-
-            if (leftIdx < size && arr[i] < arr[leftIdx])
-            {
-                swap(arr[i], arr[leftIdx]);
-                i = leftIdx;
-            }
-            else if (rightIdx < size && arr[i] < arr[rightIdx])
-            {
-                swap(arr[i], arr[rightIdx]);
-                i = rightIdx;
-            }
-            else
-            {
-                return;
-            }
-        }
-    }
-};
-
-void heapify(int arr[], int n, int i)
-{
-    int largest = i;
-    int left = 2 * i;
-    int right = 2 * i + 1;
-
-    if (left <= n && arr[largest] < arr[left])
-    {
-        largest = left;
-    }
-    if (right <= n && arr[largest] < arr[right])
-    {
-        largest = right;
-    }
-
-    if (largest != i)
-    {
-        swap(arr[largest], arr[i]);
-        heapify(arr, n, largest);
-    }
-}
-
-void heapSort(int arr[], int n)
-{
-    int size = n;
-
-    while (size > 1)
-    {
-        swap(arr[size], arr[1]);
-        size--;
-        heapify(arr, size, 1);
-    }
-}
 
 int main()
 {
-    Heap h;
-    h.insert(50);
-    h.insert(55);
-    h.insert(52);
-    h.insert(40);
-    // h.print();
-    h.deleteFromHeap();
-    // h.print();
+    unordered_map<string, int> m;
+    pair<string, int> p = make_pair("piyu", 100);
+    m.insert(p);
+    pair<string, int> pair2("praveen", 12);
+    m.insert(pair2);
 
-    int arr[6] = {-1, 54, 53, 55, 52, 50};
-    int n = 5;
-    for (int i = n / 2; i > 0; i--)
-    {
-        heapify(arr, n, i);
-    }
-    heapSort(arr, n);
-    cout << "Printing the heap: " << endl;
-    for (int i = 1; i <= n; i++)
-    {
-        cout << arr[i] << " ";
-    }
-    cout << endl;
-    // Max-heap
-    priority_queue<int> maxheap;
-    maxheap.push(5);
-    maxheap.push(6);
-    maxheap.push(3);
-    cout << maxheap.top() << endl;
-    // Min heap
-    priority_queue<int, vector<int>, greater<int>> minheap;
-    minheap.push(5);
-    minheap.push(6);
-    minheap.push(3);
-    cout << minheap.top() << endl;
+    m["romeo"] = 23;
+
+    // search 
+    cout << m["romeo"] << endl;
+    cout << m.at("piyu") << endl;
+
+    cout << m["anything"] << endl;
+    cout << m.at("anything") << endl;
+
+    //size
+
+    cout << m.size() << endl;
+
+    //to check presence
+
+    cout << m.count("piyu") << endl;
+    cout << m.count("piyuu") << endl;
+
+    //erase
+
+    m.erase("praveen");
+    cout << m.count("praveen") << endl;
     return 0;
 }
